73d7c0c79b3cf42c2b0b61cf4946702f7b94e482
samarth <samarthturmari@gmail.com>
2023-09-10 13:48:34 +0530

    stored landmarks

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import cv2
 import mediapipe as mp
 import time
+import numpy as np
 
 mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
-
+size = 10
+array = np.zeros((size, 32, 3))
 cap = cv2.VideoCapture('track_runner.mp4')
+arrayIndex = 0
 pTime = 0
 while True:
     success, img = cap.read()
+
+    if not success:
+        break
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
     # print(results.pose_landmarks)
@@ -17,9 +23,9 @@ while True:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
         for id, lm in enumerate(results.pose_landmarks.landmark):
             h, w, c = img.shape
-            print(id, lm)
+            #print(id, lm)
             cx, cy = int(lm.x * w), int(lm.y * h)
-            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+            array[arrayIndex, id] = {cx, cy, lm.z}
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
@@ -30,3 +36,9 @@ while True:
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+    arrayIndex = (arrayIndex+1) % size
+
+cv2.destroyAllWindows()
+
+# Release the video capture object
+cap.release()
